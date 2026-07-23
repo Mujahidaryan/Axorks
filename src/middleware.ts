f3a317ea418c534be6a093a578dc87a212a8a4fb
mujahidaryan222149@gmail.com <mujahidaryan222149@gmail.com>
2026-07-23 10:33:00 +0500

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,8 @@ export function middleware(request: NextRequest) {
   const host = request.headers.get('host') || '';
   const url = request.nextUrl.clone();
 
-  // 301 Permanent Redirect from axorks.vercel.app or www.axorks.com to axorks.com
-  if (host === 'axorks.vercel.app' || host === 'www.axorks.com') {
+  // Redirect www.axorks.com to axorks.com
+  if (host === 'www.axorks.com') {
     url.host = 'axorks.com';
     url.port = '';
     url.protocol = 'https';
@@ -18,13 +18,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public assets (png, jpg, svg)
-     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
